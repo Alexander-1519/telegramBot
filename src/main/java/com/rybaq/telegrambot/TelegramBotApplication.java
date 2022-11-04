0b1cf76be92ebc5ef5dd736c1073def5be2183a6
alexander.rybak <alexander.rybak2020@mail.ru>
2022-11-04 10:43:57 +0300

    categories

--- src/main/java/com/rybaq/telegrambot/TelegramBotApplication.java
+++ src/main/java/com/rybaq/telegrambot/TelegramBotApplication.java
@@ -2,11 +2,13 @@ package com.rybaq.telegrambot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @SpringBootApplication
+@EnableScheduling
 public class TelegramBotApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/rybaq/telegrambot/scheduler/AppScheduler.java
@@ -0,0 +1,13 @@
+package com.rybaq.telegrambot.scheduler;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppScheduler {
+
+    @Scheduled(cron = "0 0/5 * * * ?")
+    public void appScheduler() {
+        System.out.println("5 minutes late...");
+    }
+}
